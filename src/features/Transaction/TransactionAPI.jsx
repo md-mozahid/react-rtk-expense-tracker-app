bd43672dda17527769637748c8b825a5ae4c3d25
mozahid <ce.muzahid@gmail.com>
2023-07-09 21:30:59 +0600

    added axios

---
+++ src/features/Transaction/TransactionAPI.jsx
@@ -0,0 +1,21 @@
+import axiosInstance from '../../utils/Axios'
+
+export const getTransactions = async () => {
+  const response = await axiosInstance.get('/transactions')
+  return response.data
+}
+
+export const addTransactions = async (data) => {
+  const response = await axiosInstance.post('/transactions', data)
+  return response.data
+}
+
+export const editTransactions = async (id, data) => {
+  const response = await axiosInstance.put(`/transactions/${id}`, data)
+  return response.data
+}
+
+export const deleteTransactions = async (id) => {
+  const response = await axiosInstance.delete(`/transactions/${id}`)
+  return response.data
+}
